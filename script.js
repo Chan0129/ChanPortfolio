b9fe70742a46f1f39fb0e6efca0d0fb342f888ef
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-06-24 23:29:50 +0800

    Add weather app

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+window.onload = function () {
+  document.getElementById("desktop-nav").classList.add("fade-in");
+  document.getElementById("hamburger-nav").classList.add("fade-in");
+};
+document.addEventListener("DOMContentLoaded", function () {
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  const fadeInOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  document.querySelectorAll(".fade-in-section").forEach((section) => {
+    fadeInOnScroll.observe(section);
+  });
+});
